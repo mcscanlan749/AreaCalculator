43e8e4b6d106c62590cf9fe81fed18504d9a08f9
mcscanlan749 <mcscanlan749@gmail.com>
2021-03-29 18:20:01 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace AreaCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the length of the room: ");
+            string length = Console.ReadLine();
+            decimal lengthNum = decimal.Parse(length);
+
+            Console.Write("Enter the width of the room: ");
+            string width = Console.ReadLine();
+            decimal widthNum = decimal.Parse(width);
+
+            Console.Write("Enter the overage percent: ");
+            string overage = Console.ReadLine();
+            decimal overageInt = int.Parse(overage);
+            decimal overagePercent = overageInt / 100;
+
+            decimal area = lengthNum * widthNum;
+            decimal overageAmount = area * overagePercent;
+            decimal total = area + overageAmount;
+
+            Console.Write($"The total area, including overage, is: {total}");
+
+
+
+        }
+    }
+}
